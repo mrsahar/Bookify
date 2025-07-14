06b70bc042ec9245b93842f053b49c725016593d
mrsaharg <winsahar5@gmail.com>
2025-07-14 23:22:54 +0500

    genrePages and controller added

---
+++ Bookify/Controllers/GenreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Controllers
+{
+    public class GenreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
