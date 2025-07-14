4151d08c23896d339e67ce41f624952232e164bf
mrsaharg <winsahar@outlook.com>
2025-07-15 00:11:29 +0500

    new pages

--- Bookify/Controllers/AuthorController.cs
+++ Bookify/Controllers/AuthorController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookify.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Controllers
 {
     public class AuthorController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Author model)
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
         {
             return View();
         }
